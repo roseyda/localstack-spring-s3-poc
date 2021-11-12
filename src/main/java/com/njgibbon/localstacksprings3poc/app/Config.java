d0f969745e579b245aba6c198739f2ce1adf3d14
David ROSEY <david.rosey.pro@gmail.com>
2021-11-12 14:51:03 +0100

    fix style

--- src/main/java/com/njgibbon/localstacksprings3poc/app/Config.java
+++ src/main/java/com/njgibbon/localstacksprings3poc/app/Config.java
@@ -1,12 +1,10 @@
 package com.njgibbon.localstacksprings3poc.app;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
@@ -14,28 +12,29 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 public class Config {
-	
-	@Autowired
-	private Environment environment;
-	
-	private final static String LOCAKSTACK_PROFIL = "localstack";
-	
-	// TODO: set as properties
-	private final static String S3_SERVICE_ENDPOINT = "http://localhost:4566/";
-	private final static String S3_SERVICE_REGION= "eu-west-1";
-	private final static String NON_PROXY_HOST = "localhost|127.0.0.1|10.0.2.15";
 
-	@Bean
-	public AmazonS3 getAmazonS3() {
-		AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard()
-				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(S3_SERVICE_ENDPOINT, S3_SERVICE_REGION))
-				.withPathStyleAccessEnabled(true);
-		
-		if (Arrays.stream(environment.getActiveProfiles()).anyMatch(profile -> (LOCAKSTACK_PROFIL.equalsIgnoreCase(profile)))) {
-			builder.withClientConfiguration(new ClientConfiguration()
-					.withNonProxyHosts(NON_PROXY_HOST));
-		}
-		
-		return builder.build();
-	}
+  @Autowired
+  private Environment environment;
+
+  private final static String LOCAKSTACK_PROFIL = "localstack";
+
+  // TODO: set as properties
+  private final static String S3_SERVICE_ENDPOINT = "http://localhost:4566/";
+  private final static String S3_SERVICE_REGION = "eu-west-1";
+  private final static String NON_PROXY_HOST = "localhost|127.0.0.1|10.0.2.15";
+
+  @Bean
+  public AmazonS3 getAmazonS3() {
+    AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard()
+        .withEndpointConfiguration(
+            new AwsClientBuilder.EndpointConfiguration(S3_SERVICE_ENDPOINT, S3_SERVICE_REGION))
+        .withPathStyleAccessEnabled(true);
+
+    if (Arrays.stream(environment.getActiveProfiles())
+        .anyMatch(profile -> LOCAKSTACK_PROFIL.equalsIgnoreCase(profile))) {
+      builder.withClientConfiguration(new ClientConfiguration().withNonProxyHosts(NON_PROXY_HOST));
+    }
+
+    return builder.build();
+  }
 }
--- src/main/java/com/njgibbon/localstacksprings3poc/app/Controller.java
+++ src/main/java/com/njgibbon/localstacksprings3poc/app/Controller.java
@@ -6,14 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
@@ -26,96 +24,96 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 @RequestMapping(value = "/", produces = "text/plain")
 public class Controller {
 
-	@Autowired
-	private AmazonS3 s3;
+  @Autowired
+  private AmazonS3 s3;
 
-	@ResponseBody
-	@GetMapping("/")
-	public String main() {
-		return "localstack-spring-s3-poc";
-	}
+  @ResponseBody
+  @GetMapping("/")
+  public String main() {
+    return "localstack-spring-s3-poc";
+  }
 
-	@ResponseBody
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World!";
-	}
+  @ResponseBody
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello World!";
+  }
 
-	@ResponseBody
-	@GetMapping("/listBuckets")
-	public String listBuckets() {
-		return stringifyListBuckets();
-	}
+  @ResponseBody
+  @GetMapping("/listBuckets")
+  public String listBuckets() {
+    return stringifyListBuckets();
+  }
 
-	@ResponseBody
-	@GetMapping("/listObjects")
-	public String listObjects(@RequestParam("bucketName") String bucketName) {
-		return stringifyListObjects(bucketName);
-	}
+  @ResponseBody
+  @GetMapping("/listObjects")
+  public String listObjects(@RequestParam("bucketName") String bucketName) {
+    return stringifyListObjects(bucketName);
+  }
 
-	@ResponseBody
-	@GetMapping("/readObject")
-	public String readObject(@RequestParam("bucketName") String bucketName,
-			@RequestParam("objectName") String objectName) {
-		return stringifyContentsOfBucketObject(bucketName, objectName);
-	}
+  @ResponseBody
+  @GetMapping("/readObject")
+  public String readObject(@RequestParam("bucketName") String bucketName,
+      @RequestParam("objectName") String objectName) {
+    return stringifyContentsOfBucketObject(bucketName, objectName);
+  }
 
-	public String stringifyContentsOfBucketObject(String bucketName, String objectName) {
-		String returnString = "";
+  public String stringifyContentsOfBucketObject(String bucketName, String objectName) {
+    String returnString = "";
 
-		try {
-			S3Object o = s3.getObject(new GetObjectRequest(bucketName, objectName));
-			returnString = readTextInputStream(o.getObjectContent());
-		} catch (AmazonServiceException e) {
-			returnString = "oops";
-			System.err.println(e.getErrorMessage());
-		} catch (FileNotFoundException e) {
-			returnString = "oops";
-			System.err.println(e.getMessage());
-		} catch (IOException e) {
-			returnString = "oops";
-			System.err.println(e.getMessage());
-		}
-		return returnString;
-	}
+    try {
+      S3Object o = s3.getObject(new GetObjectRequest(bucketName, objectName));
+      returnString = readTextInputStream(o.getObjectContent());
+    } catch (AmazonServiceException e) {
+      returnString = "oops";
+      System.err.println(e.getErrorMessage());
+    } catch (FileNotFoundException e) {
+      returnString = "oops";
+      System.err.println(e.getMessage());
+    } catch (IOException e) {
+      returnString = "oops";
+      System.err.println(e.getMessage());
+    }
+    return returnString;
+  }
 
-	public String stringifyListBuckets() {
-		String returnString = "";
+  public String stringifyListBuckets() {
+    String returnString = "";
 
-		List<Bucket> buckets = s3.listBuckets();
-		System.out.println("Your Amazon S3 buckets are:");
-		for (Bucket b : buckets) {
-			System.out.println("* " + b.getName());
-			returnString = returnString + b.getName() + " ";
-		}
+    List<Bucket> buckets = s3.listBuckets();
+    System.out.println("Your Amazon S3 buckets are:");
+    for (Bucket b : buckets) {
+      System.out.println("* " + b.getName());
+      returnString = returnString + b.getName() + " ";
+    }
 
-		return returnString;
-	}
+    return returnString;
+  }
 
-	public String stringifyListObjects(String bucketName) {
-		String returnString = "";
+  public String stringifyListObjects(String bucketName) {
+    String returnString = "";
 
-		ListObjectsV2Result result = s3.listObjectsV2(bucketName);
-		List<S3ObjectSummary> objects = result.getObjectSummaries();
-		for (S3ObjectSummary os : objects) {
-			System.out.println("* " + os.getKey());
-			returnString = returnString + os.getKey() + " ";
-		}
+    ListObjectsV2Result result = s3.listObjectsV2(bucketName);
+    List<S3ObjectSummary> objects = result.getObjectSummaries();
+    for (S3ObjectSummary os : objects) {
+      System.out.println("* " + os.getKey());
+      returnString = returnString + os.getKey() + " ";
+    }
 
-		return returnString;
-	}
+    return returnString;
+  }
 
-	private String readTextInputStream(InputStream input) throws IOException {
-		String returnString = "";
-		// Read the text input stream one line at a time and display each line.
-		// And concat
-		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			System.out.println(line);
-			returnString = returnString + line + " ";
-		}
-		System.out.println();
-		return returnString;
-	}
+  private String readTextInputStream(InputStream input) throws IOException {
+    String returnString = "";
+    // Read the text input stream one line at a time and display each line.
+    // And concat
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      System.out.println(line);
+      returnString = returnString + line + " ";
+    }
+    System.out.println();
+    return returnString;
+  }
 }
